a8861665d8a9e57a1b45db76dbb0b161d5e36737
kevin <kevinochieng548@gmail.com>
2017-01-31 06:48:35 +1300

    test title page

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,6 +4,7 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select "title", "Home | ProjectTracker"
   end
 
 end
