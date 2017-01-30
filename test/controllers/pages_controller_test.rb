04417e94fae7992f58c3a71482515234ad724ef5
kevin <kevinochieng548@gmail.com>
2017-01-31 06:51:24 +1300

    setup common title

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,10 +1,13 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @common_title = "| ProjectTracker"
+  end
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Home | ProjectTracker"
+    assert_select "title", "Home "
   end
 
 end
