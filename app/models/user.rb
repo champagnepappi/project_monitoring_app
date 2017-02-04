cd8a42201e63d6be8eb4e6c98c849733cacf02f0
kevin <kevinochieng548@gmail.com>
2017-02-04 21:20:24 +0300

    validate reg no

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   before_save {self.email = email.downcase}
   validates :first_name, presence: true, length: {maximum: 40}
   validates :last_name, presence: true, length: {maximum: 40}
+  validates :reg_no, presence: true, length: {maximum: 20}
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
      validates :email, presence: true,length: {maximum: 100},
            format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
