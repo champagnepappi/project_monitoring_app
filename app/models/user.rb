eed6b3ada3500662d75c02ecf4846e4f7e9741b8
kevin <kevinochieng548@gmail.com>
2017-02-04 21:15:47 +0300

    ensure uniqueness by downcasing email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
   validates :first_name, presence: true, length: {maximum: 40}
   validates :last_name, presence: true, length: {maximum: 40}
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
