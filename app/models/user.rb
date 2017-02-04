17fe979f0a43becac0daf34130df386f725e2b3f
kevin <kevinochieng548@gmail.com>
2017-02-04 21:22:43 +0300

    validate department and course taken

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   validates :last_name, presence: true, length: {maximum: 40}
   validates :reg_no, presence: true, length: {maximum: 20}
   validates :gender, presence: true
+  validates :department, presence: true
+  validates :course_taken, presence: true
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
      validates :email, presence: true,length: {maximum: 100},
            format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
