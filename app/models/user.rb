795003753d351ced1f34d3747e8b17cd8d04a6fd
kevin <kevinochieng548@gmail.com>
2017-02-04 21:08:00 +0300

    validate first name and last name length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, presence: true, length: {maximum: 40}
+  validates :last_name, presence: true, length: {maximum: 40}
   DEPARTMENT = ["IT", "Computer Science"]
   COURSE_TAKEN = ["IT", "Computer Science", "Network Security", "Information System"]
 
