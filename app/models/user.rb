c66394fd55d907fe9a85751ac34237309554197e
kevin <kevinochieng548@gmail.com>
2017-02-04 21:03:26 +0300

    validate presence of first name and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   DEPARTMENT = ["IT", "Computer Science"]
   COURSE_TAKEN = ["IT", "Computer Science", "Network Security", "Information System"]
 
