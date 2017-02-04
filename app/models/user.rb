78cf3c70427e3f59f1e6faf09eb2208c66a74dd1
kevin <kevinochieng548@gmail.com>
2017-02-04 21:11:51 +0300

    validate email and format with email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   validates :first_name, presence: true, length: {maximum: 40}
   validates :last_name, presence: true, length: {maximum: 40}
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+     validates :email, presence: true,length: {maximum: 100},
+           format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
   DEPARTMENT = ["IT", "Computer Science"]
   COURSE_TAKEN = ["IT", "Computer Science", "Network Security", "Information System"]
 
