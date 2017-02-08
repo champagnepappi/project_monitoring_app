48a9b8bc067b18b49496517501a280a67904b877
kevin <kevinochieng548@gmail.com>
2017-02-08 22:47:42 +0300

    test presence for first name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,6 +2,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(first_name: "Just Anonymous", email: "dummy@gmail.com")
+    @user = User.new(first_name: "Justin",last_name: "Anonymous", email: "dummy@gmail.com",
+                   reg_no: "COM/0040/13", gender: "male", department: "IT", course_taken: "IT" )
+  end
+
+  test "first_name should be present" do
+    @user.first_name = ""
+    assert_not @user.valid?
   end
 end
