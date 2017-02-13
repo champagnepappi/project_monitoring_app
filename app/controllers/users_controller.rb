404c448d0c5b2c94e8ce8cca7118655b0eff9875
kevin <kevinochieng548@gmail.com>
2017-02-13 19:54:55 +0300

    define user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :reg_no, :gender, :department, :course_taken, :password, 
+                                :password_confirmation)
+  end
 end
