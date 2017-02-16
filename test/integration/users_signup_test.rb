9e51bbfb425c32109fdff69830b085973df24b52
kevin <kevinochieng548@gmail.com>
2017-02-17 00:22:47 +0300

    test signup with invalid info

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid signup" do
+    get new_user_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          first_name: "",
+          last_name: "",
+          email: "dida@g.com",
+          reg_no: "COM/0311/13",
+          gender: "Male",
+          department: "IT",
+          course_taken: "IT",
+          password: "eqedq",
+          password_confirmation: ""
+
+        }
+      }
+    end
+    assert_template 'users/new'
+  end
 end
