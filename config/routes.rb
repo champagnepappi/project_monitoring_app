b1b848e64988e7963c0e1e508615a0abba7fb876
kevin <kevinochieng548@gmail.com>
2017-02-20 19:12:05 +0300

    add named routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
-
   root 'pages#home'
+  get 'users/new'
+  
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
