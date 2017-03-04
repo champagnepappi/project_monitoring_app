0164d04339c46ae95ecc3feb62feaec0e3b8dc37
kevin <kevinochieng548@gmail.com>
2017-03-04 17:28:33 +0300

    test login without remembering

--- test/integration/login_test.rb
+++ test/integration/login_test.rb
@@ -46,4 +46,9 @@ class LoginTest < ActionDispatch::IntegrationTest
     log_in_as(@user, remember_me: '1')
     assert_not_nil cookies['remember_token']
   end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
 end
