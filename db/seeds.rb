c2d538788b8b5e398499c4a92b9408f8426122d2
kevin <kevinochieng548@gmail.com>
2017-03-05 12:21:03 +0300

    add and activate seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin = User.create!(
+  first_name: "Kevyne",
+  last_name: "Santos",
+  email: "santos@gmail.com",
+  reg_no: "COM/0040/13",
+  gender: "Male",
+  department: "IT",
+  course_taken: "IT",
+  password: "morty",
+  password_confirmation: "morty",
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
+)
+
+user = User.create!(
+  first_name: "Jane",
+  last_name: "Sharon",
+  email: "sharon@gmail.com",
+  reg_no: "SIT/0020/13",
+  gender: "Female",
+  department: "IT",
+  course_taken: "IT",
+  password: "tyrant",
+  password_confirmation: "tyrant",
+  admin: false,
+  activated: true,
+  activated_at: Time.zone.now
+)
