b32ded13e7fad48cb0c5586aba3b664cc4dc2cb0
kevin <kevinochieng548@gmail.com>
2017-03-05 11:41:04 +0300

    add attribute accessor activation token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   before_save {self.email = email.downcase}
   validates :first_name, presence: true, length: {maximum: 40}
   validates :last_name, presence: true, length: {maximum: 40}
