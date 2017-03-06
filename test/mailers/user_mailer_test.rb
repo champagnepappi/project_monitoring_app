d0d309dd1b7d4e6749aa302f612d276321169596
kevin <kevinochieng548@gmail.com>
2017-03-06 13:02:23 +0300

    fix error in UserMailer test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,10 +5,10 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:karam)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
+    assert_equal "Account Activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
-    assert_match user.name,                     mail.body.encoded
+    assert_match user.first_name,                     mail.body.encoded
     assert_match user.activation_token,         mail.body.encoded
     assert_match CGI::escape(user.email),       mail.body.encoded
   end
