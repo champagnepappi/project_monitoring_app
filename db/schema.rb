8434b1018f88d6fe713cffdb854b84a81c4f75f0
kevin <kevinochieng548@gmail.com>
2017-03-06 11:26:56 +0300

    add admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304144058) do
+ActiveRecord::Schema.define(version: 20170306082504) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170304144058) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.boolean  "admin"
   end
 
 end
