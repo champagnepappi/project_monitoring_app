06be36ee2ca7135ccd0ed6f99834e29869e2179f
kevin <kevinochieng548@gmail.com>
2017-03-11 15:08:15 +0300

    define private method logged_in_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,11 @@ class UsersController < ApplicationController
     params.require(:user).permit(:first_name, :last_name, :email, :reg_no, :gender, :department, :course_taken, :password, 
                                 :password_confirmation)
   end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
 end
