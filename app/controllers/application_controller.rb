359631ef98bed6d6402445d9da02ccf9cdcb66d5
kevin <kevinochieng548@gmail.com>
2017-03-13 15:00:50 +0300

    add already signed in method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def already_signed_in
+    if current_user
+      redirect_to root_url
+      flash[:info] = "You are already signed in"
+    end
+  end
 end
