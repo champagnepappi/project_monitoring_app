d303703bbf47cc7b637aeda3e97baaa1e0f1626e
kevin <kevinochieng548@gmail.com>
2017-03-13 12:55:38 +0300

    test unsuccessful edit

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:karam)
+  end
+
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: {
+      user: {
+          first_name: "",
+          last_name: "angrlo",
+          email: "dida@g.com",
+          reg_no: "COM/0011/13",
+          gender: "Male",
+          department: "IT",
+          course_taken: "Computer Science",
+          password: "",
+          password_confirmation: "qwertyu"
+      }
+    }
+    assert_template 'users/edit'
+  end
 end
