0c2cf1dcd1efbe78fea84794f4a267d37cc83a21
kevin <kevinochieng548@gmail.com>
2017-03-13 13:03:06 +0300

    successful edit

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -24,4 +24,25 @@ class UserEditTest < ActionDispatch::IntegrationTest
     }
     assert_template 'users/edit'
   end
+
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: {
+      user: {
+          first_name: "mike",
+          last_name: "angrlo",
+          email: "dida@g.com",
+          reg_no: "COM/0011/13",
+          gender: "Male",
+          department: "IT",
+          course_taken: "Computer Science",
+          password: "qwertyu",
+          password_confirmation: "qwertyu"
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to @user
+  end
 end
