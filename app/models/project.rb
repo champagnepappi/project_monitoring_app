a518123c036bb4e5f92ad03c37cfeaba62c02ce2
kevin <kevinochieng548@gmail.com>
2017-03-14 20:56:54 +0300

    validate title and description

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 5 }, presence: true
 end
