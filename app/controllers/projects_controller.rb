69e5d729f07bd07f5d0616f7c11f8c747ba2f66a
kevin <kevinochieng548@gmail.com>
2017-03-14 21:32:17 +0300

    define private method project params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,13 @@
 class ProjectsController < ApplicationController
   def new
+    @project = Project.new
+  end
+
+  def create
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :description)
   end
 end
