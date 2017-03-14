bb4721dfadbd1945e5ae04251fd7eefc41ce5d19
kevin <kevinochieng548@gmail.com>
2017-03-14 20:51:31 +0300

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308095411) do
+ActiveRecord::Schema.define(version: 20170314174826) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
