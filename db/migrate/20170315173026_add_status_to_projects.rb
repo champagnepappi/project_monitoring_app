eb5ed5a65c792387c81668598bcdd957d5251d29
kevin <kevinochieng548@gmail.com>
2017-03-15 20:33:43 +0300

    add default status

--- db/migrate/20170315173026_add_status_to_projects.rb
+++ db/migrate/20170315173026_add_status_to_projects.rb
@@ -1,5 +1,5 @@
 class AddStatusToProjects < ActiveRecord::Migration[5.0]
   def change
-    add_column :projects, :status, :integer
+    add_column :projects, :status, :integer, default: 0
   end
 end
