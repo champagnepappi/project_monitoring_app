282007929511c7ce301fa84dadefe002bf3b4bb7
kevin <kevinochieng548@gmail.com>
2017-03-15 20:24:34 +0300

    add enum status to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+  enum status: { submitted: 0, approved: 1, rejected: 2, finished: 3 }
   belongs_to :user
   validates :title, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 10 }, presence: true
