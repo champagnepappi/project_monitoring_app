b4edde50518f598699d16ceaf42e32c48ef44482
kevin <kevinochieng548@gmail.com>
2017-03-15 20:56:49 +0300

    add named route for approve

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
+  get 'approve' => 'projects#approve'
   resources :users
   resources :projects
   resources :account_activations, only: [:edit]
