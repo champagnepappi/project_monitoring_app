ea748365d2b05c9a7f2b9b1ff27bba6b43c870d2
kevin <kevinochieng548@gmail.com>
2017-03-18 21:18:12 +0300

    test invalid project submission

--- test/integration/project_submission_test.rb
+++ test/integration/project_submission_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ProjectSubmissionTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:karam)
+  end
+
+  test "invalid project submission" do
+    log_in_as(@user)
+    get new_project_path
+    post projects_path, params: {
+      project: {
+        title: "",
+        description: ""
+      }
+    }
+    assert_template 'projects/new'
+  end
 end
