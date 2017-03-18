059ec7a1990ca59c7ffbbc2f1f634e875091a968
kevin <kevinochieng548@gmail.com>
2017-03-18 21:08:53 +0300

    modify projects controller test

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:karam)
+  end
+
   test "should get new" do
+    log_in_as(@user)
     get projects_new_url
     assert_response :success
   end
