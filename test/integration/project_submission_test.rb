ff3158ccbb1ca0ddf9bb6777b4d3d87eacea23d8
kevin <kevinochieng548@gmail.com>
2017-03-18 21:24:42 +0300

    test valid project submission

--- test/integration/project_submission_test.rb
+++ test/integration/project_submission_test.rb
@@ -16,4 +16,18 @@ class ProjectSubmissionTest < ActionDispatch::IntegrationTest
     }
     assert_template 'projects/new'
   end
+
+  test "valid project submission" do
+    log_in_as(@user)
+    get new_project_path
+    post projects_path, params: {
+      project: {
+        title: "chat application",
+        description: "wehwrijwjrwjjgwjgwi9u3y49qhqrgdjjfcvsfbsdiuiriueueiriw"
+      }
+    }
+    assert_redirected_to @user 
+    assert_not flash.empty?
+    assert_equal flash[:success], "Project successfully submitted"
+  end
 end
