0de78fe497b048c6ce5fbd31ecc02602c31a831f
kevin <kevinochieng548@gmail.com>
2017-03-22 21:27:24 +0300

    ensure uniqueness in project title

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   enum status: { submitted: 0, approved: 1, rejected: 2, finished: 3 }
   belongs_to :user
-  validates :title, length: { minimum: 5 }, presence: true
+  validates :title, length: { minimum: 5 }, presence: true, uniqueness: true
   validates :description, length: { minimum: 10 }, presence: true
 end
