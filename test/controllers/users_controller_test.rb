e29102fd69546aa6fd5ac1d5a1f5f4afc6d8421f
kevin <kevinochieng548@gmail.com>
2017-03-24 22:09:24 +0300

    test redirect for edit when not logged in

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:karam)
+  end
+
   test "should get new" do
     get users_new_url
     assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
 end
