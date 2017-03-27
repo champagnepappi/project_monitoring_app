8c54d020a57b57de07f1c36a5d1da9b811b61972
kevin <kevinochieng548@gmail.com>
2017-03-27 17:27:58 +0300

    define permitted attributes method

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,7 +1,14 @@
 class UserPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope
+  # class Scope < Scope
+  #   def resolve
+  #     scope
+  #   end
+  # end
+  def permitted_attributes
+    if user.lecturer
+      [:first_name, :last_name, :email,:password, :password_confirmation]
+    else
+      [:first_name, :last_name, :email, :gender, :course_taken, :department, :password, :password_confirmation]
     end
   end
 end
