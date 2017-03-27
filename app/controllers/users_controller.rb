0457f5fbda01b49c7402ccfe9fba8be5ecf1a142
kevin <kevinochieng548@gmail.com>
2017-03-27 17:29:56 +0300

    modify user params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,8 +52,9 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :reg_no, :gender, :department, :course_taken, :password, 
-                                :password_confirmation)
+    # params.require(:user).permit(:first_name, :last_name, :email, :reg_no, :gender, :department, :course_taken, :password, 
+    #                             :password_confirmation)
+    params.require(:user).permit(permitted_params)
   end
 
   def lec_params
