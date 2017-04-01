6f7b16965fcb0e75b4045ab608bda18cfe8a013b
kevin <kevinochieng548@gmail.com>
2017-04-01 12:32:28 +0300

    generate model Lecturer

---
+++ db/migrate/20170401093214_create_lecturers.rb
@@ -0,0 +1,12 @@
+class CreateLecturers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :lecturers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
