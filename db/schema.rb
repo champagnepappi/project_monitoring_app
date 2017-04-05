c228c24181b32353c1436023f252a23e6deec342
kevin <kevinochieng548@gmail.com>
2017-04-05 20:39:35 +0300

    add password digest to lecturers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404175119) do
+ActiveRecord::Schema.define(version: 20170405173704) do
 
   create_table "lecturers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
+    t.string   "password_digest"
   end
 
   create_table "projects", force: :cascade do |t|
