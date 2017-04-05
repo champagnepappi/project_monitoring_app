bccf5f62a04db43e1d20fc0a690103f10b74d9ca
kevin <kevinochieng548@gmail.com>
2017-04-05 21:00:35 +0300

    validate first and last name with name regex

--- app/models/lecturer.rb
+++ app/models/lecturer.rb
@@ -1,3 +1,6 @@
 class Lecturer < ApplicationRecord
   has_secure_password
+  name_regex = /\A[a-z]+\Z/i
+  validates :first_name, presence: true, length: {maximum: 40}, format: {with: name_regex}
+  validates :last_name, presence: true, length: {maximum: 40}, format: {with: name_regex}
 end
