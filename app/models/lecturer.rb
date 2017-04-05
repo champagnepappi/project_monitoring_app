44fd8730ae35a5724416d3bcd5644a7377e1215a
kevin <kevinochieng548@gmail.com>
2017-04-05 21:02:30 +0300

    validate email

--- app/models/lecturer.rb
+++ app/models/lecturer.rb
@@ -1,6 +1,11 @@
 class Lecturer < ApplicationRecord
-  has_secure_password
   name_regex = /\A[a-z]+\Z/i
   validates :first_name, presence: true, length: {maximum: 40}, format: {with: name_regex}
   validates :last_name, presence: true, length: {maximum: 40}, format: {with: name_regex}
+
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+     validates :email, presence: true,length: {maximum: 100},
+           format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+  has_secure_password
 end
