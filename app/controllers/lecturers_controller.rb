3ff478afb4747971f4d5fcbf63a49805d095adf4
kevin <kevinochieng548@gmail.com>
2017-04-05 20:00:52 +0300

    define create action for sad path

--- app/controllers/lecturers_controller.rb
+++ app/controllers/lecturers_controller.rb
@@ -3,6 +3,14 @@ class LecturersController < ApplicationController
     @lec = Lecturer.new
   end
 
+  def create
+    @lec = Lecturer.new(lec_params)
+    if @lec.save
+    else
+      render 'new'
+    end
+  end
+
   private
   def lec_params
     params.require(:lecturer).permit(:first_name, :last_name, :email, :password, :password_confirmation)
