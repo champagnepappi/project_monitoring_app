d53d72e6daac5bfe4917be3980f6e3f2cfb3a677
kevin <kevinochieng548@gmail.com>
2017-04-12 12:51:25 +0300

    test for available links after logout

--- test/integration/lec_login_test_test.rb
+++ test/integration/lec_login_test_test.rb
@@ -35,6 +35,14 @@ class LecLoginTestTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", lecturer_path(@lec)
     assert_select "a[href=?]", logout_path
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", lecturer_path(@lec),count: 0
+    assert_select "a[href=?]", logout_path, count: 0
+    
   end
 
 end
