55387ed94b8d1b1f22bc77c9c4611e468a034394
kevin <kevinochieng548@gmail.com>
2017-04-14 11:45:05 +0300

    modify current lec method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,7 +17,16 @@ module SessionsHelper
   end
 
   def current_lec
-    Lecturer.find_by(id: session[:user_id])
+    # Lecturer.find_by(id: session[:user_id])
+    if(user_id = session[:user_id])
+      @current_lec ||= Lecturer.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      lec = Lecturer.find_by(id: user_id)
+      if lec && lec.authenticated?(:remember, cookies[:remember_token])
+        log_in lec
+        @current_lec = lec
+      end
+    end
   end
 
   def current_user?(user)
