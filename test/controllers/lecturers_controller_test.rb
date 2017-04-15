417a9d85f098de97ba6e6e543690e776be875fb1
kevin <kevinochieng548@gmail.com>
2017-04-15 20:10:56 +0300

    redirect lecturer try to edit when not logged in

--- test/controllers/lecturers_controller_test.rb
+++ test/controllers/lecturers_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class LecturersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @lec = lecturers(:ritah)
+  end
+
   test "should get new" do
     get lecturers_new_url
     assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_lecturer_path(@lec)
+    assert_not flash.empty?
+    assert_redirected_to login_path
   end
 
 end
