dcb3c91dffad1d863466e31b9284765d582380a1
kevin <kevinochieng548@gmail.com>
2017-04-18 15:52:01 +0300

    define sessions method createlec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,12 @@ class SessionsController < ApplicationController
 
   def new
   end
+  
+  def new_lec
+  end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase) ||
-       Lecturer.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase) 
     if user && user.authenticate(params[:session][:password]) 
       if user.activated? 
         log_in user 
@@ -23,6 +25,25 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
+  def createlec
+    lec = Lecturer.find_by(email: params[:session_lec][:email].downcase) 
+    if lec && lec.authenticate(params[:session_lec][:password]) 
+      if lec.activated? 
+        log_in lec 
+        params[:session_lec][:remember_me] == '1' ? remember(lec) : forget(lec)
+        redirect_back_or lec 
+      else
+        message = "Account not activated"
+        message += "Check your email for activation link"
+        flash[:warning] = message
+        redirect_to root_url
+      end
+    else
+      flash.now[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
+  end
+
 
   def destroy
     log_out if logged_in?
