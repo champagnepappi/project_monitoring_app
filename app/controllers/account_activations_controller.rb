88f67abca28b6b6d4f16c3fade2f18a04030a62c
kevin <kevinochieng548@gmail.com>
2017-04-19 13:26:15 +0300

    define editlec method

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,13 +1,20 @@
 class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email]) 
-      lec = Lecturer.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
       flash[:success] = "Account activated"
       redirect_to user
-    elsif lec && lec.activated? && lec.authenticated?(:activation, params[:id])
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
+
+  def editlec
+      lec = Lecturer.find_by(email: params[:email])
+    if lec && !lec.activated? && lec.authenticated?(:activation, params[:id])
       lec.activate
       login lec
       flash[:success] = "Account activated"
