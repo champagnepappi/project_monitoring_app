859776ce2cc1ae3e5e5ec26ae031906629158cb0
kevin <kevinochieng548@gmail.com>
2017-04-19 22:17:52 +0300

    define full_name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,10 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
