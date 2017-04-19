1ca97b72ad74a42b97c9d796daba02d790ea210b
kevin <kevinochieng548@gmail.com>
2017-04-19 16:07:32 +0300

    modify account activation edit action

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,20 +1,13 @@
 class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email]) 
+      lec = Lecturer.find_by(email: params[:lec_email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
       flash[:success] = "Account activated"
       redirect_to user
-    else
-      flash[:danger] = "Invalid activation link"
-      redirect_to root_url
-    end
-  end
-
-  def editlec
-      lec = Lecturer.find_by(email: params[:email])
-    if lec && !lec.activated? && lec.authenticated?(:activation, params[:id])
+    elsif lec && !lec.activated? && lec.authenticated?(:activation, params[:id])
       lec.activate
       login lec
       flash[:success] = "Account activated"
@@ -24,4 +17,5 @@ class AccountActivationsController < ApplicationController
       redirect_to root_url
     end
   end
+
 end
