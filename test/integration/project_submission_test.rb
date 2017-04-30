ff8969f73ec16f44311df727afb168a03b22d1d9
kevin <kevinochieng548@gmail.com>
2017-04-30 22:02:02 +0300

    setup second fixture user

--- test/integration/project_submission_test.rb
+++ test/integration/project_submission_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ProjectSubmissionTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:karam)
+    @user2 = users(:glorious)
   end
 
   test "invalid project submission" do
@@ -29,5 +30,8 @@ class ProjectSubmissionTest < ActionDispatch::IntegrationTest
     assert_redirected_to @user 
     assert_not flash.empty?
     assert_equal flash[:success], "Project successfully submitted"
+  end
+
+  test "wrong user submitting project" do
   end
 end
