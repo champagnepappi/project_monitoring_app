7f92fb3030a9ab7b63e9d5c71b2156a8896e8a7f
kevin <kevinochieng548@gmail.com>
2017-05-03 12:31:11 +0300

    add resource bids

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   resources :users
   resources :lecturers
   resources :projects
+  resources :bids
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
 
