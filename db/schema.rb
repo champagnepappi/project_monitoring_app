9452c2bdeed237ac47069cee8ee3fb8e984b4a49
kevin <kevinochieng548@gmail.com>
2017-05-03 18:52:46 +0300

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419170000) do
+ActiveRecord::Schema.define(version: 20170503154555) do
+
+  create_table "bids", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "lecturer_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["lecturer_id"], name: "index_bids_on_lecturer_id"
+    t.index ["user_id"], name: "index_bids_on_user_id"
+  end
 
   create_table "lecturers", force: :cascade do |t|
     t.datetime "created_at",                        null: false
