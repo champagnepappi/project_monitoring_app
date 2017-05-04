2b4085fc5035e40896256a9a51a51b0255b6607c
kevin <kevinochieng548@gmail.com>
2017-05-04 21:23:16 +0300

    define private method bid params

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -2,4 +2,9 @@ class BidsController < ApplicationController
   def new 
     @bid = Bid.new
   end
+
+  private
+  def bid_params
+    params.require(:bid).permit(:user_id, :lecturer_id)
+  end
 end
