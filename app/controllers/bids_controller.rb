b6e2afe14f707e6af4c90370354e757fc53c5fa6
kevin <kevinochieng548@gmail.com>
2017-05-05 11:02:43 +0300

    define bids create action

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -3,6 +3,18 @@ class BidsController < ApplicationController
     @bid = Bid.new
   end
 
+  def create
+    @bid = Bid.new(bid_params)
+    student = User.find_by(id: params[:user_id])
+    if @bid.save
+      redirect_to users_path
+      student.update_attribute(:status, "assigned")
+    else
+      flash[:danger] = "There was a problem assigning the student"
+      redirect_to supervisor_path
+    end
+  end
+
   private
   def bid_params
     params.require(:bid).permit(:user_id, :lecturer_id)
