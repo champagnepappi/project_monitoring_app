9fed6923650256e34b2d27111f410762a0da7f3e
kevin <kevinochieng548@gmail.com>
2017-05-06 21:32:52 +0300

    add relationship model validations

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
 class Relationship < ApplicationRecord
   belongs_to :supervisor, class_name: "Lecturer"
   belongs_to :supervised, class_name: "User"
+  validates :supervisor_id, presence: true
+  validates :supervised_id, presence: true
 end
