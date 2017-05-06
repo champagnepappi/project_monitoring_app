ce7e1077882d75390da4e9f18c02c8612b9f557f
kevin <kevinochieng548@gmail.com>
2017-05-06 13:13:58 +0300

    add default status

--- db/migrate/20170506101221_add_status_to_lecturers.rb
+++ db/migrate/20170506101221_add_status_to_lecturers.rb
@@ -1,5 +1,5 @@
 class AddStatusToLecturers < ActiveRecord::Migration[5.0]
   def change
-    add_column :lecturers, :status, :integer
+    add_column :lecturers, :status, :integer, default: 0
   end
 end
