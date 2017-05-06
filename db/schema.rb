c4b09bc5c58a2c5c5a1a9f53fb7a4c67cd5c691d
kevin <kevinochieng548@gmail.com>
2017-05-06 20:59:28 +0300

    generate model Relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506101221) do
+ActiveRecord::Schema.define(version: 20170506175843) do
 
   create_table "bids", force: :cascade do |t|
     t.integer  "user_id"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20170506101221) do
     t.datetime "updated_at",              null: false
     t.integer  "status",      default: 0
     t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "supervisor_id"
+    t.integer  "supervised_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
