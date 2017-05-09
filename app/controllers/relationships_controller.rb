f7d109d04dcf825822820a93f6c70d55f172611e
kevin <kevinochieng548@gmail.com>
2017-05-09 21:08:17 +0300

    define relationships create action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,3 +1,10 @@
 class RelationshipsController < ApplicationController
   before_action :logged_in_user
+
+  def create
+    student = User.find_by(id: params[:supervised_id])
+    lec = Lecturer.find_by(id: params[:supervisor_id])
+    lec.supervise(student)
+    redirect_to users_path
+  end
 end
