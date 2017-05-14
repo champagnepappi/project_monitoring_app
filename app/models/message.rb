77f4224f1f4c9a61ca71d20d8ce42b342f7f5ac6
kevin <kevinochieng548@gmail.com>
2017-05-14 18:40:25 +0300

    order messages with default scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: { maximum: 140 }
   validates :content, presence: true
 end
