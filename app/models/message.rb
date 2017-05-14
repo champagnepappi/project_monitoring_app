99bdcb3714e350ec76084da952e7c2ca480cb0e5
kevin <kevinochieng548@gmail.com>
2017-05-14 18:41:36 +0300

    validate user_id presence

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: { maximum: 140 }
-  validates :content, presence: true
+  validates :user_id, presence: true
 end
