a58c7c378574fcd812c3960b2b98721cc0bd48e8
kevin <kevinochieng548@gmail.com>
2017-05-15 15:42:53 +0300

    add correct user before filter

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @message = current_user.messages.build(message_params)
