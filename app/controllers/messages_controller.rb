244765458d9f4cd2d1c705577e7da2aadf25adf5
kevin <kevinochieng548@gmail.com>
2017-05-15 13:44:13 +0300

    define message create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,15 @@
 class MessagesController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   
+  def create
+    @message = current_user.messages.build(message_params)
+    if @message.save
+      flash[:success] = "Your message was successfully sent!"
+      redirect_to current_user
+    else
+      flash[:danger] = "There was a problem submitting message"
+      redirect_to current_user
+    end
+  end
 end
+
