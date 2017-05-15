1878ea8093cded1c8b9269c6797c8ddc24d1eb9b
kevin <kevinochieng548@gmail.com>
2017-05-15 20:37:44 +0300

    add picture validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,12 @@ class Message < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
+  validate :picture_size
+
+  private
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
