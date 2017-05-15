24bb6fc6a2f581e4adae033fa3b674bf79dd935d
kevin <kevinochieng548@gmail.com>
2017-05-15 20:20:34 +0300

    add image to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
+  mount_uploader :picture, PictureUploader
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
