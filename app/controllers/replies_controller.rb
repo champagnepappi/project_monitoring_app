818cb7f828e2cdbdbe4db6eb9471ab25797c8967
kevin <kevinochieng548@gmail.com>
2017-05-16 18:32:22 +0300

    define create action

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,2 +1,12 @@
 class RepliesController < ApplicationController
+  def create
+    @reply = Reply.new(message_params)
+    if @reply.save
+      flash[:success] = "Your message was successfully sent!"
+      redirect_to current_lec
+    else
+      flash[:danger] = "There was a problem submitting message"
+      redirect_to current_lec
+    end
+  end
 end
