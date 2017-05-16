0eb9719a02317744ba7fb7496049fc024bdfa5c7
kevin <kevinochieng548@gmail.com>
2017-05-16 18:47:38 +0300

    validate content presence

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,7 @@
 class Reply < ApplicationRecord
   belongs_to :message
   belongs_to :lecturer
+  validates :content, presence: true, length: { maximum: 140 }
   default_scope -> { order(created_at: :desc) }
   validates :lecturer_id, presence: true
   validates :message_id, presence: true
