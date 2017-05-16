333791cf7b5d6334d977411e75f02d9f78577ae7
kevin <kevinochieng548@gmail.com>
2017-05-16 14:44:00 +0300

    order replys with default scope

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,7 @@
 class Reply < ApplicationRecord
   belongs_to :message
   belongs_to :lecturer
+  default_scope -> { order(created_at: :desc) }
   validates :lecturer_id, presence: true
   validates :message_id, presence: true
 end
