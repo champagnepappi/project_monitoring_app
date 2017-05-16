c34e7160d101b97461491a1c22017de6b66c3470
kevin <kevinochieng548@gmail.com>
2017-05-16 19:01:14 +0300

    add image to reply

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,7 @@
 class Reply < ApplicationRecord
   belongs_to :message
   belongs_to :lecturer
+  mount_uploader :picture, PictureUploader
   validates :content, presence: true, length: { maximum: 140 }
   default_scope -> { order(created_at: :desc) }
   validates :lecturer_id, presence: true
