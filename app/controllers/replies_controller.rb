af67f2e541a6410c69dab04468247f01aaae7ccd
kevin <kevinochieng548@gmail.com>
2017-05-16 18:34:45 +0300

    define private methods

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,6 +1,6 @@
 class RepliesController < ApplicationController
   def create
-    @reply = Reply.new(message_params)
+    @reply = Reply.new(reply_params)
     if @reply.save
       flash[:success] = "Your message was successfully sent!"
       redirect_to current_lec
@@ -8,5 +8,15 @@ class RepliesController < ApplicationController
       flash[:danger] = "There was a problem submitting message"
       redirect_to current_lec
     end
+  end
+
+  private
+  def reply_params
+    params.require(:reply).permit(:content)
+  end
+
+  def correct_user
+    @reply = current_lec.replies.find_by(id: params[:id])
+    redirect_to root_url if @reply.nil?
   end
 end
