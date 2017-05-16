4f8fb2c9d9eed899996bd957c649460bd8d480ca
kevin <kevinochieng548@gmail.com>
2017-05-16 14:42:13 +0300

    associate message with replys

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
+  has_many :replys
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :content, presence: true, length: { maximum: 140 }
