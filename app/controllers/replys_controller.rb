76cd88ffff9a5005b53554e242634fd75016f0bc
kevin <kevinochieng548@gmail.com>
2017-05-16 14:55:55 +0300

    define reply create action

--- app/controllers/replys_controller.rb
+++ app/controllers/replys_controller.rb
@@ -1,3 +1,14 @@
 class ReplysController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @reply = current_lec.replys.build(reply_params)
+    if @reply.save
+      flash[:success] = "Your reply was successfully sent"
+      redirect_to current_lec
+    else
+      flash[:danger] = "There was a problem submitting your reply"
+      redirect_to current_lec
+    end
+  end
 end
