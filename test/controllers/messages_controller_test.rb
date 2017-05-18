175451d0abc36781a3954f22a790920a7978f0b1
kevin <kevinochieng548@gmail.com>
2017-05-18 21:14:47 +0300

    test redirect for message creation if not logged in

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should redirected create when not logged in" do
+    assert_no_difference 'Message.count' do
+      post messages_path, params: {
+        message: {
+          content: "Just dummy shit"
+        }
+      }
+    end
+  end
 end
