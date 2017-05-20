4846befea53372a5c44533581a4d219c8d9a8667
kevin <kevinochieng548@gmail.com>
2017-05-20 21:39:45 +0300

    redirect create if not logged in

--- test/integration/user_messages_test.rb
+++ test/integration/user_messages_test.rb
@@ -5,4 +5,15 @@ class UserMessagesTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:karam)
   end
+
+  test "redirect create if not logged in" do
+    assert_no_difference 'Message.count' do
+      post messages_path, params: {
+        message: {
+          content: "This is a simple message"
+        }
+      }
+    end
+    assert_redirected_to login_path
+  end
 end
