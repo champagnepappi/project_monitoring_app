e00d501d97b57b38b28d2d57f21ed3ba06d4e4c0
kevin <kevinochieng548@gmail.com>
2017-05-20 20:55:20 +0300

    setup fixture user

--- test/integration/user_messages_test.rb
+++ test/integration/user_messages_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserMessagesTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:karam)
+  end
 end
