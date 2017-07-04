daea35649fc4a105c9524602da75e90ad9b6eff7
kevin <kevinochieng548@gmail.com>
2017-07-04 22:47:36 +0300

    setup fixture user

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:karam)
+  end
   
   test "should redirected create when not logged in" do
     assert_no_difference 'Message.count' do
