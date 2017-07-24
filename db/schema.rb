58e68585fc0b4045d6ee6d2fe849448158a737a7
kevin <kevinochieng548@gmail.com>
2017-07-24 12:26:21 +0300

    add gem carrierwave-video

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516164743) do
+ActiveRecord::Schema.define(version: 20170723135036) do
 
   create_table "bids", force: :cascade do |t|
     t.integer  "user_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170516164743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "picture"
+    t.string   "video"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
