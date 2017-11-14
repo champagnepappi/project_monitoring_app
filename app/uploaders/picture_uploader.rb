cdc2c5f47febb95592d9903bb6c2ad2d81d9a3e3
kevin <kevinochieng548@gmail.com>
2017-11-14 13:38:49 +0300

    define required transformations before displaying image

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -10,6 +10,14 @@ class PictureUploader < CarrierWave::Uploader::Base
   process :convert => 'png'
   process :tags => ['post_picture']
 
+  version :standard do
+    process :resize_to_fill => [100,150, :north]
+  end
+
+  version :thumbnail do
+    resize_to_fit(50,50)
+  end
+
   if Rails.env.production?
     storage :fog
   else
