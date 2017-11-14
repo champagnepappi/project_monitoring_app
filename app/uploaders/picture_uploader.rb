46392b1a56dd46945a7a0416a4b05cdeac917bec
kevin <kevinochieng548@gmail.com>
2017-11-14 13:54:30 +0300

    set eager to true

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -11,12 +11,18 @@ class PictureUploader < CarrierWave::Uploader::Base
   process :tags => ['post_picture']
 
   version :standard do
+    process :eager => true
     process :resize_to_fill => [100,150, :north]
   end
 
   version :thumbnail do
+    eager
     resize_to_fit(50,50)
   end
+
+  # def public_id
+  #   return model.content
+  # end
 
   if Rails.env.production?
     storage :fog
