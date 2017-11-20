fe501eba87df61c3a5b47ce1809b441ff61a87d0
kevin <kevinochieng548@gmail.com>
2017-11-20 15:12:37 +0300

    define the year options method

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,4 +4,8 @@ class Payment < ApplicationRecord
   def self.month_options
     Date::MONTHNAMES.compact.each_width_index.map { |name, i| ["#{i+1} - #{name}", i+1]}
   end
+
+  def self.year_options
+    (Date.today.year..(Date.today+10)).to_a
+  end
 end
