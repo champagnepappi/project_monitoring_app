98c388d77773a4484082115906c4a3c103815b2d
kevin <kevinochieng548@gmail.com>
2017-11-20 14:27:44 +0300

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723135036) do
+ActiveRecord::Schema.define(version: 20171120112304) do
 
   create_table "bids", force: :cascade do |t|
     t.integer  "user_id"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 20170723135036) do
     t.string   "picture"
     t.string   "video"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.string   "email"
+    t.string   "token"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
