fc89ee85b99e2e703dc420efe648632e8ecfc132
kevin <kevinochieng548@gmail.com>
2017-11-20 14:55:43 +0300

    define the month options method

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,7 @@
 class Payment < ApplicationRecord
   belongs_to :user
+
+  def self.month_options
+    Date::MONTHNAMES.compact.each_width_index.map { |name, i| ["#{i+1} - #{name}", i+1]}
+  end
 end
