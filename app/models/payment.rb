9b68464d6e2aace88061d4b5a57ea6b1581ce9ef
kevin <kevinochieng548@gmail.com>
2017-11-21 11:40:26 +0300

    define process payment method

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -8,4 +8,12 @@ class Payment < ApplicationRecord
   def self.year_options
     (Date.today.year..(Date.today+10)).to_a
   end
+
+  def process_payment
+    customer = Stripe::Customer.create email: email, card: token
+    Stripe::Charge.create customer: customer.id,
+                          amount: 1000,
+                          description: 'Premium',
+                          currency: 'usd'
+  end
 end
