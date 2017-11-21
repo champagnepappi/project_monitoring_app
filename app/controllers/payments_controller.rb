bce4af6ecc76cb3671b1b7b72c650724fbe13030
kevin <kevinochieng548@gmail.com>
2017-11-21 12:24:47 +0300

    setup customer

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,12 @@
 class PaymentsController < ApplicationController
   def new
   end
+
+  def create
+    @amount = 500
+    customer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
+  end
 end
